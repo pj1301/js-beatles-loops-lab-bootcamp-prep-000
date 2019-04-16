68e78cb3fdc40691fd6847ee91bc2a0b48867517
James Hayward <james.hayward6@icloud.com>
2019-04-16 19:43:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ function theBeatlesPlay(musicians, instruments) {
   } return BeatlesArray; //return function
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
+
 function johnLennonFacts(facts) {
   var factsArray [];
-  
+  while 
 }
