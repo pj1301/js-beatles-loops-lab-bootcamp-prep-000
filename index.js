11a07b891ed2e2fb0094dfdeb12690476ff698b3
James Hayward <james.hayward6@icloud.com>
2019-04-16 19:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 
 function theBeatlesPlay(musicians, instruments) {
-  var BeatlesArray = [];
-  for (let i = 0; i < musicians.length; i++) {
-    BeatlesArray.push(`${musicians[i]} plays ${instruments[i]}`); //you have to use ${} in order to ensure you don't output "musicians plays instruments"
-  } return BeatlesArray;
+  var BeatlesArray = []; // create an empty array
+  for (let i = 0; i < musicians.length; i++) { //set up a for loop to iterate the list, set it to the length of the existing list(s)
+    BeatlesArray.push(`${musicians[i]} plays ${instruments[i]}`); //link the lists to the iteration - you have to use ${} in order to ensure you don't output "musicians plays instruments"
+  } return BeatlesArray; //return function
 }
