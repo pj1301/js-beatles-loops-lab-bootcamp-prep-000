a241f344c9e45d9a477f5b46834061e4e5c05030
James Hayward <james.hayward6@icloud.com>
2019-04-16 19:35:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // add solution here
+//function theBeatlesPlay(musicians, instruments) {
+//  let BeatlesArray = [];
+//  for (let i = 0; i < 4; i++) {
+//    BeatlesArray[i] = {
+//      musicians + i + 1,
+//      "plays" instruments + i + 1
+//    }
+//  } return BeatlesArray
+//}
+
 function theBeatlesPlay(musicians, instruments) {
-  let BeatlesArray = [];
-  for (let i = 0; i < 4; i++) {
-    BeatlesArray[i] = {
-      musicians + i + 1,
-      "plays" instruments + i + 1
-    }
-  } return BeatlesArray
-}
+  var BeatlesArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    BeatlesArray.push(musicians[i] 'plays' instruments[i])
