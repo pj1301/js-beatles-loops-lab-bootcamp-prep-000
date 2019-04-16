9b64a0e754579d50c7177ea21ca0f62003587fa1
James Hayward <james.hayward6@icloud.com>
2019-04-16 19:36:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var BeatlesArray = [];
   for (let i = 0; i < musicians.length; i++) {
-    BeatlesArray.push(musicians[i] 'plays' instruments[i]);
+    BeatlesArray.push(`musicians[i] plays instruments[i]``);
   } return BeatlesArray;
 }
