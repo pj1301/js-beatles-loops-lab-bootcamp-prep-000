75bde876672a4b8b8d104ed35e00f20b72ad0286
James Hayward <james.hayward6@icloud.com>
2019-04-16 19:40:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var BeatlesArray = [];
   for (let i = 0; i < musicians.length; i++) {
-    BeatlesArray.push(`${musicians[i]} plays ${instruments[i]}`);
+    BeatlesArray.push(`${musicians[i]} plays ${instruments[i]}`); //you have to use ${} in order to ensure you don't output "musicians plays instruments"
   } return BeatlesArray;
 }
